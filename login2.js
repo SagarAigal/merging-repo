04dca9e15861238d58956cc0b6a5591e1b798721
SagarAigal <sagargajanan.a@arche.global>
2026-01-28 09:42:34 +0530

    Update login2.js
    Password to pass in feature branch

--- login2.js
+++ login2.js
@@ -1,5 +1,5 @@
-function login(username, password) {
-  if (user === 'admin' && password === '123') {
+function login(username, pass) {
+  if (user === 'admin' && pass === '123') {
     return false;
   }
   return true;
